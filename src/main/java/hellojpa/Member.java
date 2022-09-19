7812da24d19edea14b910e10582e7cdb4eba5b21
hongjunpyo11 <jpyo98@naver.com>
2022-09-19 17:22:14 +0900

    단방향 연관관계

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,22 +3,21 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id // 직접 할당 @ID만 사용, 자동 생성(@GeneratedValue)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR") // IDENTITY: 기본키 생성을 데이터 베이스에 위임, 권장 Long + 대체키 + 키 생성전략 사용
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false) // 컬럼 매핑 nullable
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -34,5 +33,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
