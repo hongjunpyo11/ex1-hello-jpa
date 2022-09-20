f3077feb825b18b25759165b6aaf1cc9f503364c
hongjunpyo11 <jpyo98@naver.com>
2022-09-20 18:58:23 +0900

    다대다 [N:M] 가이드

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -1,32 +1,19 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Product {
-    /*
-    다대다 매핑의 한계
-    편리해 보이지만 실무에서 사용 X
-    연결 테이블이 단순히 연결만 하고 끝나지 않음
-    주문시간, 수량 같은 데이터가 들어올 수 있음
-
-    다대다의 한계 극복
-    연결 테이블용 엔티티 추가
-    @ManyToMany -> @OneToMany, @ManyToOne
-     */
 
     @Id @GeneratedValue
     private Long id;
 
     private String name;
 
-    @ManyToMany(mappedBy = "products")
-    private List<Member> members = new ArrayList<>();
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
