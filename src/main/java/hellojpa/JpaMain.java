f833d454064bc0ca9b43f08c47c30cb57c2b62ce
hongjunpyo11 <jpyo98@naver.com>
2022-10-05 22:51:56 +0900

    값 타입과 불변 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
+
+            /** 한계
+             * 임베디드같은 값타입을 여러 엔티티에서 공유하면 위험함
+             * 값 타입의 실제 인스턴스인 값을 공유하는 것은 위험
+             * 대신 값(인스턴스)를 복사해서 사용
+             */
 
             tx.commit();
         } catch (Exception e) {
