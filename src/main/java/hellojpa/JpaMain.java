7d7dc4675853e67c2712023ae04dbaee4ea06c64
hongjunpyo11 <jpyo98@naver.com>
2022-10-14 00:33:33 +0900

    Criteria 소개

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,11 @@
 package hellojpa;
 
+import org.hibernate.Criteria;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -15,14 +20,17 @@ public class JpaMain {
 
         try {
 
-            List<Member> result = em.createQuery(
-                    "select  m from Member  m where m.username like '%kim%'",
-                    Member.class
-            ).getResultList();
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            for (Member member : result) {
-                System.out.println("member = " + member);
-            }
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+
+
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
@@ -36,20 +44,10 @@ public class JpaMain {
     }
 }
 
-/**
- * JPQL 소개
- * JPA를 사용하면 엔티티 객체를 중심으로 개발
- * 문제는 검색 쿼리
- * 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
- * 모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능
- * 애플릭케이션이 필요한 데이터만 DB에 불러오려면 결국 검색 조건이 포함된 SQL이 필요
+/** Criteria 소개
+ *  * 문자가 아닌 자바코드로 JPQL을 작성할 수 있음
+ *  * JPQL 빌더 역할
  *
- * JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
- * SQL 문법과 유사
- * JPQL은 엔티티 객체를 대상으로 쿼리
- * SQL은 데이터베이스 테이블을 대상으로 쿼리
- *
- * 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
- * SQL을 추상화해서 특정 데이터베이스 SQL에 의존 X
- * JPQL을 한마디로 정의하면 객체 지향 SQL
+ *  단점: 너무 복잡하고 실용성이 없다.
+ *  Criteria 대신 QueryDSL 사용 권장
  */
